5555cfba70b9c88bb70917e8a9337a35a4f63cdd
Aidan Weeks <20106513@wit.ie>
2026-09-23 10:04:37 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -18,7 +18,8 @@ function App() {
     const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority:""
   });
 
     const doneHandler = (taskIndex) => {
@@ -47,6 +48,9 @@ function App() {
       case "deadline":
           form.deadline = event.target.value;
           break;
+          case "priority":
+          form.priority = event.target.value;
+          break;
       default:
           form = formState;
     }
@@ -67,8 +71,6 @@ function App() {
   }
 
 
-
-
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -77,6 +79,7 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      priority={task.priority}
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,7 +1,12 @@
 const Task = (props) => {
     
     return (
-        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : props.priority === 'high'
+                    ? '#ff9999'
+                    : props.priority === 'medium'
+                    ? '#ffcc66'
+                    : '#99cc99'}}
+                    >
            <p>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
@@ -11,7 +16,6 @@ const Task = (props) => {
 
         </div>
     )
-    
 }
 
 export default Task;
